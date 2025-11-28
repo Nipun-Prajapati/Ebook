ca6fcc1fa87c059151e20803f7c362a94d69657f
Nipun Prajapati <nipunsprajapati@gmail.com>
2025-11-28 10:41:35 +0530

    add login router with error handler

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -3,7 +3,6 @@ import createHttpError from "http-errors";
 import User from "./userModel.ts";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
 const { sign } = jwt;
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -15,6 +14,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   // Database call && find user
+
   try {
     const findUser = await User.findOne({ email: email });
     if (findUser) {
@@ -28,11 +28,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(500, "Error while getting user."));
   }
 
-  // Hash a password
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  // store a data in database && token generation from JWT
+  // Hash a password && store a data in database && token generation from JWT
   try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await User.create({
       name,
       email,
@@ -48,4 +47,44 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    const error = createHttpError(400, "All fields are required!");
+    return next(error);
+  }
+
+  try {
+    const checkUser = await User.findOne({ email });
+
+    if (!checkUser) {
+      const error = createHttpError(400, "User not found !");
+      return next(error);
+    }
+
+    const isMatch = await bcrypt.compare(password, checkUser.password);
+
+    if (!isMatch) {
+      const error = createHttpError(
+        400,
+        "User name and Password are incorrect!"
+      );
+      return next(error);
+    }
+
+    const token = sign(
+      { sub: checkUser._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(400, "Error in logged in!"));
+  }
+};
+
+export { createUser, loginUser };
